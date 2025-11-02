8da70484d0b72c7fe0463fa54b5f6da4f4d1e902
pxffzz <ansaranwari1234@gmail.com>
2025-11-02 12:44:34 -0500

    Create register-sw.js

---
+++ lumi/vu/register-sw.js
@@ -0,0 +1,38 @@
+"use strict";
+/**
+ * Distributed with Ultraviolet and compatible with most configurations.
+ */
+const stockSW = "/lumi/uv-sw.js";
+
+/**
+ * List of hostnames that are allowed to run serviceworkers on http:
+ */
+const swAllowedHostnames = ["localhost", "127.0.0.1", "45.90.12.122"];
+
+/**
+ * Global util
+ * Used in 404.html and index.html
+ */
+async function registerSW() {
+  if (
+    location.protocol !== "https:" &&
+    !swAllowedHostnames.includes(location.hostname)
+  )
+    throw new Error("Service workers cannot be registered without https.");
+
+  if (!navigator.serviceWorker)
+    throw new Error("Your browser doesn't support service workers.");
+
+  // Ultraviolet has a stock `sw.js` script.
+  await navigator.serviceWorker.register(stockSW, {
+    scope: __uv$config.prefix,
+  });
+}
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  if (url.pathname.endsWith('embed.html')) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+});
